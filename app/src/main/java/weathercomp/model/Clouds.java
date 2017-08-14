be6e259b5f3c054a407860184a362018d1a200f6
imnavin <navin.abeysundera@gmail.com>
2017-08-14 23:17:02 +0530

    Added classes in model package

---
+++ app/src/main/java/weathercomp/model/Clouds.java
@@ -0,0 +1,19 @@
+package weathercomp.model;
+
+/**
+ * Created by navin on 8/14/2017.
+ */
+
+public class Clouds {
+
+    private int precipitation;
+
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(int precipitation) {
+        this.precipitation = precipitation;
+    }
+
+}
