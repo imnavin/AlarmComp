0cf92a48f4caa472bd7578ea9e991100002608c3
imnavin <navin.abeysundera@gmail.com>
2017-10-16 02:50:19 +0530

    Created the classes in the mapscomp package.

---
+++ app/src/main/java/mapscomp/DataParser.java
@@ -0,0 +1,8 @@
+package mapscomp;
+
+/**
+ * Created by navin on 10/16/2017.
+ */
+
+public class DataParser {
+}
---
+++ app/src/main/java/mapscomp/GetNearbyPlacesData.java
@@ -0,0 +1,9 @@
+package mapscomp;
+
+/**
+ * Created by navin on 10/16/2017.
+ */
+
+public class GetNearbyPlacesData {
+
+}
